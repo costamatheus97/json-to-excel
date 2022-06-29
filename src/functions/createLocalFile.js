1c271538fdfea72e8fe3a4a31419d3184f47a5b9
Matheus Costa <costamatheus97@live.com>
2022-06-29 16:18:09 -0300

    fix: path

--- src/functions/createLocalFile.js
+++ src/functions/createLocalFile.js
@@ -1,7 +1,9 @@
 const { createWorkbook } = require('./createWorkbook')
+const fs = require('fs')
 
 const createLocalFile = async (path, worksheetName, jsonData) => {
-  const workbook = createWorkbook(worksheetName, jsonData)
+  const workbook = await createWorkbook(worksheetName, jsonData)
+  await fs.promises.mkdir(path, { recursive: true })
 
   await workbook.xlsx.writeFile(`${path}/${Date.now()}.xlsx`);
 }
